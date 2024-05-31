7dd9d1c23c1fc8f8193179d313db3b5b8a35986d
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-05-31 16:17:54 -0600

    styles: apply cover image

--- app/modules/json-placeholder/(screens)/index.tsx
+++ app/modules/json-placeholder/(screens)/index.tsx
@@ -1,19 +1,20 @@
-import { StyleSheet } from "react-native";
+import CoverImage from "@/ui/components/CoverImage";
 import ParallaxScrollView from "@/ui/components/ParallaxScrollView";
-import { Ionicons } from "@expo/vector-icons";
-import { ThemedView } from "@/ui/components/ThemedView";
 import { ThemedText } from "@/ui/components/ThemedText";
+import { ThemedView } from "@/ui/components/ThemedView";
+import { StyleSheet } from "react-native";
+
+const image = {
+  source: require("@/assets/images/jsonplaceholder-logo.png"),
+  blurhash: "LHNwm3?aIpt7~qt6D*j[oIRjt5WB",
+};
 
 export default function Index() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
       headerImage={
-        <Ionicons
-          size={310}
-          name="information-circle"
-          style={styles.headerImage}
-        />
+        <CoverImage source={image.source} blurhash={image.blurhash} />
       }
     >
       <ThemedView style={styles.titleContainer}>
@@ -32,12 +33,6 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
-  },
   titleContainer: {
     flexDirection: "row",
     gap: 8,
